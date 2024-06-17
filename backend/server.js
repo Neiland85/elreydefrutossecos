63da7a615f5fe65ea1a730703b5972b033f373b3
Neiland85 <theia3impact@gmail.com>
2024-06-17 19:55:33 +0200

    Updated server.js to include complete configuration for MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,12 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config();
+dotenv.config(); // Carga las variables de entorno desde el archivo .env
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
+// Middleware para procesar JSON y habilitar CORS
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -19,16 +19,20 @@ const mongoUri = process.env.MONGO_URI;
 
 if (!mongoUri) {
     console.error("No se encontró la variable MONGO_URI en el archivo .env");
-    process.exit(1);
+    process.exit(1); // Sale de la aplicación si no se encuentra la URI
 }
 
 mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Conectado a la base de datos'))
-    .catch(err => console.log(err));
+    .catch(err => {
+        console.error("Error conectando a la base de datos:", err);
+        process.exit(1); // Sale de la aplicación si hay un error en la conexión
+    });
 
-// Rutas
-app.use('/api', require('./routes/api'));
+// Definición de rutas
+app.use('/api', require('./routes/api')); // Asegúrate de que la ruta a 'api.js' es correcta
 
+// Iniciar el servidor
 app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`);
 });
