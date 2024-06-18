96d98dbc03a68545bbef9d3b1f49824c363af629
Neiland85 <theia3impact@gmail.com>
2024-06-18 23:17:44 +0200

    Configured Parcel and Tailwind CSS, added React dependencies

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// tailwind.config.js
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.js'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
